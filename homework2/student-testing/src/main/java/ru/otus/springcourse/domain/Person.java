eb77b566e8fc06380aac09bc9205fdb9182e072c
m.kozlovskiy <m.kozlovskiy@tinkoff.ru>
2022-06-18 12:35:08 +0300

    homework2 delete redundant methods

--- homework2/student-testing/src/main/java/ru/otus/springcourse/domain/Person.java
+++ homework2/student-testing/src/main/java/ru/otus/springcourse/domain/Person.java
@@ -8,23 +8,6 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     @Override
     public String toString() {
         return  firstName + " " + lastName;
--- homework2/student-testing/src/main/java/ru/otus/springcourse/domain/Question.java
+++ homework2/student-testing/src/main/java/ru/otus/springcourse/domain/Question.java
@@ -5,8 +5,6 @@ public class Question {
     private String text;
     private String correctAnswer;
 
-    private String personAnswer;
-
     public Question(int id, String text, String correctAnswer) {
         this.id = id;
         this.text = text;
@@ -17,31 +15,12 @@ public class Question {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getText() {
         return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getPersonAnswer() {
-        return personAnswer;
-    }
-
-    public void setPersonAnswer(String personAnswer) {
-        this.personAnswer = personAnswer;
     }
 
     public String getCorrectAnswer() {
         return correctAnswer;
     }
 
-    public void setCorrectAnswer(String correctAnswer) {
-        this.correctAnswer = correctAnswer;
-    }
 }
